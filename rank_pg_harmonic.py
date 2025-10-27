89a264955e20492afe66e8c5e512b9e92362736c
Matteo Loporchio <mloporchio@users.noreply.github.com>
2025-10-27 14:30:37 +0000

    New ranking scripts

--- rank_pg_harmonic.py
+++ rank_pg_harmonic.py
@@ -11,7 +11,7 @@ SIZE = int(sys.argv[1])
 OUTPUT_PATH = sys.argv[2]
 TRANSACTION_MAP_FILE = "/data/backup/safeBTC/mapTxHash2Ids.csv"
 NODE_MAP_FILE = "pg/nm.tsv"
-HARMONIC_FILE = "pg/harmonic.tsv"
+HARMONIC_FILE = "download/harmonic/pg.txt"
 OUTPUT_FILE = "pg_harmonic.tsv"
 
 df_tx_map = pl.read_csv(TRANSACTION_MAP_FILE, separator=",", has_header=False, new_columns=['tx_hash', 'tx_id'])
@@ -22,7 +22,8 @@ df_node_map = (
     )
 ).select("tx_id", "output_id", "node_id").join(df_tx_map, on="tx_id", how="left").drop("tx_id") # tx_hash, output_id, node_id
 
-df = pl.read_csv(HARMONIC_FILE, separator="\t").join(df_node_map, on='node_id', how='left')
+df = (pl.read_csv(HARMONIC_FILE, separator="\t", has_header=False, new_columns=['node_id', 'harmonic'])
+      .join(df_node_map, on='node_id', how='left'))
 
 top = df.sort("harmonic", descending=True).head(SIZE).select("node_id", "tx_hash", "output_id", "harmonic").with_row_index("rank")
 top.write_csv(OUTPUT_PATH + "/" + OUTPUT_FILE, null_value="null", separator="\t")
---
+++ run_rank.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#   Script to run ranking computations for different graph models.
+#   Author: Matteo Loporchio
+#
+
+MEASURE=$1
+MEASURES=("degree" "pagerank" "clustering" "harmonic")
+SIZE=10000 # Number of entries in the final ranking
+MODELS=("ag" "tg" "ug" "atg" "pg") # List of model names
+OUTPUT_DIR="rank" # Output directory where results will be saved
+
+# Create output directory if it does not exist.
+mkdir -p ${OUTPUT_DIR}
+
+# Check if the provided measure is valid.
+if [[ ! " ${MEASURES[@]} " =~ " ${MEASURE} " ]]; then
+    echo "Error: Invalid measure '${MEASURE}'. Valid options are: ${MEASURES[*]}"
+    exit 1
+fi
+
+for i in "${!MODELS[@]}"; do
+    MODEL=${MODELS[$i]}
+    echo "Processing ${MODEL}..."
+    SCRIPT_NAME="rank_${MODEL}_${MEASURE}.py"
+    python3 ${SCRIPT_NAME} ${SIZE} ${OUTPUT_DIR}
+    echo "Done!"
+done
